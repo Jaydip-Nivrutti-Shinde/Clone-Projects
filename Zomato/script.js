a30263b13f20254ed209996c5743b49343dd31cc
Jaydip-N-Shinde <sjaydip844@gmail.com>
2025-07-27 01:37:19 +0530

    Zomato new Clone Home Page Upated

---
+++ Zomato/script.js
@@ -0,0 +1,28 @@
+const btn1 = document.getElementById('bt1');
+const btn2 = document.getElementById('bt2');
+const btn3 = document.getElementById('bt3');
+const img1 = document.getElementById('img1');
+const img2 = document.getElementById('img2');
+const img3 = document.getElementById('img3');
+
+btn1.addEventListener('click',()=>{
+    img1.src = 'https://b.zmtcdn.com/data/o2_assets/30fa0a844f3ba82073e5f78c65c18b371616149662.png';
+    img2.src = 'https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png';
+    img3.src = 'https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png';
+});
+
+btn2.addEventListener('click',()=>{
+    img1.src = 'https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png';
+    img2.src = 'https://b.zmtcdn.com/data/o2_assets/c0bb85d3a6347b2ec070a8db694588261616149578.png';
+    img3.src = 'https://b.zmtcdn.com/data/o2_assets/01040767e4943c398e38e3592bb1ba8a1616150142.png';
+});
+
+btn3.addEventListener('click',()=>{
+    img1.src = 'https://b.zmtcdn.com/data/o2_assets/78d25215ff4c1299578ed36eefd5f39d1616149985.png';
+    img2.src = 'https://b.zmtcdn.com/data/o2_assets/246bbd71fbba420d5996452be3024d351616150055.png';
+    img3.src = 'https://b.zmtcdn.com/data/o2_assets/855687dc64a5e06d737dae45b7f6a13b1616149818.png';
+}); 
+
+
+
+
